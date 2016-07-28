f3f8af6c7370c18b44d51d331c96ecdf6ae20484
Mahati Gollamudi <github email address>
2016-07-28 09:35:18 -0400

    Anagram Lab Complete: sorted letters to match anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    anagrams = []
+    array_of_words.each do |words|
+      w = words.split("") #array of letters
+      if w.sort == @word.chars.sort
+        anagrams << words
+      end
+      #anagrams
+    end
+    anagramslear
+  end
+
+end
